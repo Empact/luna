a8a3d4f885b1bdaeaa1d7eacf9284bf0349cd15b
Ben Woosley <ben.woosley@gmail.com>
2011-10-05 23:58:32 -0700

    Generate rails app for prototyping

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Luna::Application.config.secret_token = 'aa9aa62278ca24e0771473a265dc81a85d7c63cb93138483cf445394a5f843e6702370dbcaa3815233457e5f57d37c8ba3d4a90cea7abd0ff78c8738a3c8815f'
