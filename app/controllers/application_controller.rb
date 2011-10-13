9e8162f3fcacf1bd460c58d09527378b35b0f358
Ben Woosley <ben.woosley@gmail.com>
2011-10-12 18:15:33 -0700

    Send unauthorized logged-out users to the signin page, with a return-to parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def after_sign_in_path_for(resource)
+    params[:return_to] || super
+  end
+
+  def permission_denied
+    if !user_signed_in?
+      flash.alert = "You must sign in to access that page"
+      redirect_to new_user_session_path(return_to: request.url)
+    else
+      super
+    end
+  end
 end
