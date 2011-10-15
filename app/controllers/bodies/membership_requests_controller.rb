7833b9d178bde57de5b66d3de4b76a0057f67cfd
Ben Woosley <ben.woosley@gmail.com>
2011-10-15 16:13:54 -0400

    Fix membership requests such that the body show page works for logged-out users

--- app/controllers/bodies/membership_requests_controller.rb
+++ app/controllers/bodies/membership_requests_controller.rb
@@ -2,6 +2,7 @@ class Bodies::MembershipRequestsController < ApplicationController
   filter_resource_access nested_in: :bodies
 
   def create
+    @membership_request.member = current_user
     if @membership_request.save
       flash.notice = "Successfully requested membership"
     else
