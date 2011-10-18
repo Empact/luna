77516fcbc1908217b9c7664cbca29ed5f61f4666
Ben Woosley <ben.woosley@gmail.com>
2011-10-18 14:14:25 -0400

    Fix that questions were missing their creator

--- app/controllers/groups/questions_controller.rb
+++ app/controllers/groups/questions_controller.rb
@@ -5,6 +5,7 @@ class Groups::QuestionsController < ApplicationController
   end
 
   def create
+    @question.created_by = current_user
     if @question.save
       redirect_to group_question_path(@group, @question)
     else
